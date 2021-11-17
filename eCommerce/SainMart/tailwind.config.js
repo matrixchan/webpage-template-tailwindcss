44457fb0ca5a354a43d881470e2fe066d17032cf
Matrixtech Ubuntu18.04 <immatrixchan@gmail.com>
2021-11-17 23:07:52 +0800

    container,颜色定制

--- eCommerce/SainMart/tailwind.config.js
+++ eCommerce/SainMart/tailwind.config.js
@@ -2,10 +2,23 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    screen:{
+      sm:'576px',
+      md: '768px',
+      lg: '992px',
+      xl: '1200px'
+    },
+    container:{
+      center:true,
+      padding:'1rem'
+    },
     extend: {
       fontFamily:{
         poppins:"'Poppins', sans-serif",
         roboto:"'Roboto', sans-serif"
+      },
+      colors:{
+        'primary': '#336699'
       }
     },
   },
